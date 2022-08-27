3ed580159fe694d775b4200fe015e6b896d7fffd
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-08-28 00:49:17 +0500

    code updated

--- lecture02_widgets_for_user_interface/lib/login.dart
+++ lecture02_widgets_for_user_interface/lib/login.dart
@@ -10,10 +10,42 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text("Login"),
-      ),
+    var students = ["Maryam", "eman", "Daniyal", " afnan"];
+    var age = ["20", "34", "89", " 98"];
+    var image = [
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Aptech_Limited_Logo.svg/1200px-Aptech_Limited_Logo.svg.png",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Aptech_Limited_Logo.svg/1200px-Aptech_Limited_Logo.svg.png",
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Aptech_Limited_Logo.svg/1200px-Aptech_Limited_Logo.svg.png",
+      "https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y2FyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"
+    ];
+
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: students.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(image[index]),
+                radius: 20,
+              ),
+              title: Text(students[index]),
+              subtitle: Text(age[index]),
+              trailing: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Text("2:47PM"),
+                  CircleAvatar(
+                    backgroundColor: Colors.green,
+                    radius: 10,
+                    child: Text(
+                      "56",
+                      style: TextStyle(fontSize: 10),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }),
     );
   }
 }
