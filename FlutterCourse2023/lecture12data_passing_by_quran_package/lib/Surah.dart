52e53fed36d59cb8b9e3275cd608ec3443110398
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2023-06-09 20:28:18 +0500

    audio player updated

--- FlutterCourse2023/lecture12data_passing_by_quran_package/lib/Surah.dart
+++ FlutterCourse2023/lecture12data_passing_by_quran_package/lib/Surah.dart
@@ -1,49 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:quran/quran.dart' as quran;
+import 'package:just_audio/just_audio.dart';
 
 // ignore: must_be_immutable
-
-class Surah extends StatelessWidget {
+class Surah extends StatefulWidget {
   int indexsurah;
   Surah(this.indexsurah, {super.key});
+
+  @override
+  State<Surah> createState() => _SurahState();
+}
+
+class _SurahState extends State<Surah> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  IconData playpauseButton = Icons.play_circle_fill_rounded;
+  bool isplaying = true;
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  Future<void> togglebutton() async {
+    try {
+      final audiourl = await quran.getAudioURLBySurah(widget.indexsurah + 1);
+      print(audiourl);
+      audioPlayer.setUrl(audiourl);
+
+      // if isplaying == true
+      if (isplaying) {
+        audioPlayer.play();
+
+        setState(() {
+          playpauseButton = Icons.pause_circle_rounded;
+          isplaying = false;
+        });
+      } else {
+        audioPlayer.pause();
+        setState(() {
+          playpauseButton = Icons.play_circle_fill_rounded;
+          isplaying = true;
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          quran.getSurahName(indexsurah + 1),
-          style: TextStyle(fontFamily: 'fontqur', color: Colors.black),
-        ),
-      ),
+          title: Text(
+            quran.getSurahName(widget.indexsurah + 1),
+            style: TextStyle(fontFamily: 'fontqur', color: Colors.white),
+          ),
+          backgroundColor: Colors.brown[900]),
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(15.0),
-          child: Card(
-            child: ListView.builder(
-              itemCount: quran.getVerseCount(indexsurah + 1),
-              itemBuilder: (context, index) {
-                return Card(
-                  child: ListTile(
-                    title: Text(
-                      quran.getVerse(indexsurah + 1, index + 1,
-                          verseEndSymbol: true),
-                      textAlign: TextAlign.right,
-                      style:
-                          TextStyle(fontFamily: 'fontqur', color: Colors.black),
-                    ),
-                  ),
-                  margin: const EdgeInsets.all(10),
-                  shape: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.white)),
-                );
-              },
-            ),
-            margin: const EdgeInsets.all(10),
-            shape: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: const BorderSide(color: Colors.white)),
+          child: Column(
+            children: [
+              Expanded(
+                  child: Card(
+                child: ListView.builder(
+                  itemCount: quran.getVerseCount(widget.indexsurah + 1),
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: ListTile(
+                        title: Text(
+                          quran.getVerse(widget.indexsurah + 1, index + 1,
+                              verseEndSymbol: false),
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontFamily: 'fontqur',
+                              color: Colors.brown[600]),
+                        ),
+                        leading: CircleAvatar(
+                          child: Text("${index + 1}",
+                              style: TextStyle(
+                                color: Colors.white,
+                              )),
+                          backgroundColor: Colors.brown[900],
+                        ),
+                      ),
+                      margin: const EdgeInsets.all(10),
+                      shape: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: const BorderSide(color: Colors.white)),
+                    );
+                  },
+                ),
+                margin: const EdgeInsets.all(10),
+                shape: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(color: Colors.white)),
+              )),
+              Card(
+                elevation: 6,
+                shadowColor: Colors.brown[900],
+                child: Center(
+                  child: IconButton(
+                      icon: Icon(
+                        playpauseButton,
+                        color: Colors.brown[900],
+                      ),
+                      onPressed: togglebutton),
+                ),
+              ),
+            ],
           ),
         ),
       ),
--- FlutterCourse2023/lecture12data_passing_by_quran_package/lib/Surahlist.dart
+++ FlutterCourse2023/lecture12data_passing_by_quran_package/lib/Surahlist.dart
@@ -13,26 +13,34 @@ class Surahlist extends StatefulWidget {
 class _SurahlistState extends State<Surahlist> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Surah(index),
-                    ));
-              },
-              title: Text(quran.getSurahName(index + 1),
-                  style: TextStyle(color: Colors.white)),
-              subtitle: Text(quran.getSurahNameEnglish(index + 1),
-                  style: TextStyle(color: Colors.white))),
-          color: Colors.indigo[900],
-        );
-      },
-      itemCount: quran.totalSurahCount,
-      // itemCount: 114,
-    );
+    return Scaffold(
+        appBar: AppBar(
+            title: Text(
+              "القرآن الکریم ",
+              style: TextStyle(fontFamily: 'mushaf', color: Colors.white),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.brown[900]),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Surah(index),
+                        ));
+                  },
+                  title: Text(quran.getSurahName(index + 1),
+                      style: TextStyle(color: Colors.white)),
+                  subtitle: Text(quran.getSurahNameEnglish(index + 1),
+                      style: TextStyle(color: Colors.white))),
+              color: Colors.brown[900],
+            );
+          },
+          itemCount: quran.totalSurahCount,
+          // itemCount: 114,
+        ));
   }
 }
