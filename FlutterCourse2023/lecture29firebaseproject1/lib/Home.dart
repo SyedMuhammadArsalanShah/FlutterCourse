54331ba7311883756e56ae6c8fbde880671b69b3
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2023-09-06 04:36:05 +0500

    insert data in real time firedb

--- FlutterCourse2023/lecture29firebaseproject1/lib/Home.dart
+++ FlutterCourse2023/lecture29firebaseproject1/lib/Home.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:lecture29firebaseproject1/Login.dart';
+import 'package:lecture29firebaseproject1/Toastmsg.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,24 +12,97 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+// createData
+
+  var titleController = TextEditingController();
+  var descController = TextEditingController();
+
+  showformmodel(context, int? key) async {
+    titleController.clear();
+    descController.clear();
+
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.fromLTRB(
+              32, 32, 32, MediaQuery.of(context).viewInsets.bottom),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: titleController,
+                decoration: InputDecoration(hintText: "Enter Your Title "),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: descController,
+                decoration: InputDecoration(hintText: "Enter Your Description"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+
+                    
+                            DatabaseReference dref =
+                        FirebaseDatabase.instance.ref("users");
+                           
+                         final key= FirebaseAuth.instance.currentUser!.uid;
+                         dref.child(key).child(DateTime.now().microsecondsSinceEpoch.toString()).set({
+                          "Title":titleController.text,
+                          "Description":descController.text,
+                          "Dateofpost": DateTime.now().toString()
+                         }).then((value) {Toastmsg().toast("Successfully Uploaded");})
+                         .onError((error, stackTrace) {Toastmsg().toast("${error}");});
+
+  
+                  
+
+                    Navigator.pop(context);
+                  },
+                  child: Text("Add Text")),
+              SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(actions: [IconButton( onPressed: () async {
-                await FirebaseAuth.instance.signOut().then((value) {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Login()));
-                });
-              }, icon: Icon(Icons.logout_outlined))],),
-      body: Center(
-          child: ElevatedButton(
-              onPressed: () async {
-                await FirebaseAuth.instance.signOut().then((value) {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Login()));
-                });
-              },
-              child: Text("logout"))),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showformmodel(context, null);
+          },
+          child: Icon(Icons.add),
+        ),
+        appBar: AppBar(
+          title: Text("Welcome"),
+          actions: [
+            IconButton(
+                onPressed: () async {
+                  await FirebaseAuth.instance.signOut()
+                  .then((value) {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Login()));
+                  });
+                },
+                icon: Icon(Icons.logout_outlined))
+          ],
+        ),
+        body: Center(
+          child: Container(
+            child: Text("Home Screen"),
+          ),
+        ));
   }
 }
