5432c062fa23ddb2366c9f87b4a2312d8dda7f45
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2023-09-10 01:44:47 +0500

    Alhamdulilah git add .! Auth & Realtime DB Search and CRUD

--- FlutterCourse2023/lecture29firebaseproject1/lib/Home.dart
+++ FlutterCourse2023/lecture29firebaseproject1/lib/Home.dart
@@ -18,12 +18,15 @@ class _HomeState extends State<Home> {
   DatabaseReference dref = FirebaseDatabase.instance.ref("users");
   final key = FirebaseAuth.instance.currentUser!.uid;
 
+// another child
+  int id = 0;
 // createData
 
   var titleController = TextEditingController();
   var descController = TextEditingController();
+  var searchCont = TextEditingController();
 
-  showformmodel(context, int? key) async {
+  showformmodel(context, int? postid) async {
     titleController.clear();
     descController.clear();
 
@@ -53,23 +56,38 @@ class _HomeState extends State<Home> {
               ),
               ElevatedButton(
                   onPressed: () async {
-                    final key = FirebaseAuth.instance.currentUser!.uid;
-                    dref
-                        .child(key)
-                        .child(DateTime.now().microsecondsSinceEpoch.toString())
-                        .set({
-                      "Title": titleController.text,
-                      "Description": descController.text,
-                      "Dateofpost": DateTime.now().toString()
-                    }).then((value) {
-                      Toastmsg().toast("Successfully Uploaded");
-                    }).onError((error, stackTrace) {
-                      Toastmsg().toast("${error}");
-                    });
+                    // ab agr post  id null  hogi to add wala task open hoga warna update ka
+                    if (postid == null) {
+                      
+                    
+                      id++;
+                      dref.child(key).child("$id").set({
+                        "Id": id,
+                        "Title": titleController.text,
+                        "Description": descController.text,
+                        "Dateofpost": DateTime.now().toString()
+                      }).then((value) {
+                        Toastmsg().toast("Successfully Uploaded");
+                      }).onError((error, stackTrace) {
+                        Toastmsg().toast("$error");
+                      });
+                    } else {
+                      dref.child(key).child("$postid").update({
+                        "Id": postid,
+                        "Title": titleController.text,
+                        "Description": descController.text,
+                        "Dateofpost": DateTime.now().toString()
+                      }).then((value) {
+                        Toastmsg().toast("Successfully Updated");
+                      }).onError((error, stackTrace) {
+                        Toastmsg().toast("$error");
+                      });
+                    }
 
                     Navigator.pop(context);
                   },
-                  child: Text("Add Text")),
+                  child:
+                      postid == null ? Text("Add Post") : Text("Update Post")),
               SizedBox(
                 height: 20,
               ),
@@ -104,44 +122,110 @@ class _HomeState extends State<Home> {
         ),
         body: Column(
           children: [
-            Expanded(
-                child: StreamBuilder(
-              stream: dref.child(key).onValue,
-              builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
-                if (!snapshot.hasData) {
-                  return CircularProgressIndicator();
-                } else {
-                  Map<dynamic, dynamic> map =
-                      snapshot.data!.snapshot.value as dynamic;
-                  List<dynamic> list = [];
-                  list = map.values.toList();
-
-                  return ListView.builder(
-                    itemCount: snapshot.data!.snapshot.children.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(list[index]['Title']),
-                        subtitle:
-                            Text(list[index]['Description']),
-                      );
-                    },
-                  );
-                }
-              },
-            )),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                controller: searchCont,
+                decoration: InputDecoration(
+                  labelText: "Search By Title",
+                  hintText: "Search Here"),
+                onChanged: (String value) {
+                  setState(() {});
+                },
+              ),
+            ),
             Expanded(
               child: FirebaseAnimatedList(
                 query: dref.child(key),
                 itemBuilder: (context, snapshot, animation, index) {
-                  return ListTile(
-                    title: Text(snapshot.child('Title').value.toString()),
-                    subtitle:
-                        Text(snapshot.child('Description').value.toString()),
-                  );
+                  // filter
+                  final title = snapshot.child('Title').value.toString();
+                  if (searchCont.text.isEmpty) {
+                    return ListTile(
+                      title: Text(snapshot.child('Title').value.toString()),
+                      subtitle:
+                          Text(snapshot.child('Description').value.toString()),
+                      trailing: PopupMenuButton(
+                        icon: Icon(Icons.menu_sharp),
+                        itemBuilder: (context) {
+                          return [
+                            PopupMenuItem(
+                                value: 1,
+                                child: ListTile(
+                                  onTap: () {
+                                    /* 
+                                    edit  main bhi hum logon ne delete ki tarah id get ki db se  than showform model ko de di jis se hum model bottom sheet open karwarahy hain ab chalty hain show model form pr
+                                    
+                                    */
+                                    final id =
+                                        snapshot.child('Id').value.toString();
+                                    showformmodel(
+                                      context,  int.parse(id),  );
+                                  },
+                                  leading: Icon(Icons.edit),
+                                  title: Text("Edit"),
+                                )),
+                            PopupMenuItem(
+                                value: 2,
+                                child: ListTile(
+                                  onTap: () {
+                                    /* 
+                                     jesy humne search ke liye title
+                                     ko liye tha na snapshot main se
+                                     warisha beta wesy hi hum ne id ko liya hai
+                                     or phir yeh id hum ne child ko pass karwadi and agr hum uper wali lety(int id =0) to us se hamesha humari value zero hi rehti */
+                                    final id =
+                                        snapshot.child('Id').value.toString();
+                                    dref.child(key).child(id).remove();
+
+                                    Navigator.pop(context);
+                                  },
+                                  leading: Icon(Icons.delete),
+                                  title: Text("Delete"),
+                                )),
+                          ];
+                        },
+                      ),
+                    );
+                  } else if (title.contains(searchCont.text.toString())) {
+                    return ListTile(
+                      title: Text(snapshot.child('Title').value.toString()),
+                      subtitle:
+                          Text(snapshot.child('Description').value.toString()),
+                    );
+                  } else {
+                    return Container();
+                  }
                 },
               ),
-            ),
+            )
           ],
         ));
   }
 }
+
+// Expanded(
+//           child: StreamBuilder(
+//         stream: dref.child(key).onValue,
+//         builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+//           if (!snapshot.hasData) {
+//             return CircularProgressIndicator();
+//           } else {
+//             Map<dynamic, dynamic> map =
+//                 snapshot.data!.snapshot.value as dynamic;
+//             List<dynamic> list = [];
+//             list = map.values.toList();
+
+//             return ListView.builder(
+//               itemCount: snapshot.data!.snapshot.children.length,
+//               itemBuilder: (context, index) {
+//                 return ListTile(
+//                   title: Text(list[index]['Title']),
+//                   subtitle:
+//                       Text(list[index]['Description']),
+//                 );
+//               },
+//             );
+//           }
+//         },
+//       )),
--- FlutterCourse2023/lecture29firebaseproject1/lib/main.dart
+++ FlutterCourse2023/lecture29firebaseproject1/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      // theme: ThemeData(
+      theme: ThemeData(
       //     // This is the theme of your application.
       //     //
       //     // TRY THIS: Try running your application with "flutter run". You'll see
@@ -33,9 +33,9 @@ class MyApp extends StatelessWidget {
       //     //
       //     // This works for code too, not just values: Most code changes can be
       //     // tested with just a hot reload.
-      //     // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      //     // useMaterial3: true,
-      //     ),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
       home: const SplashScreen(),
     );
   }
