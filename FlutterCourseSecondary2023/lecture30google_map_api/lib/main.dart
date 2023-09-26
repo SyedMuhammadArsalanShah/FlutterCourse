ba0bfc6165a2423b353ad586546e6acef2ba3696
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2023-09-26 23:15:48 +0500

    FourScreenCustom marker and FiveScreen CustomInfowindow

--- FlutterCourseSecondary2023/lecture30google_map_api/lib/main.dart
+++ FlutterCourseSecondary2023/lecture30google_map_api/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:lecture30google_map_api/FirstMapScreen.dart';
-import 'package:lecture30google_map_api/ThirdMapScreen.dart';
+import 'package:lecture30google_map_api/FiveMapScreen.dart';
+// import 'package:lecture30google_map_api/FirstMapScreen.dart';
+// import 'package:lecture30google_map_api/ThirdMapScreen.dart';
 // import 'package:lecture30google_map_api/SecondMapScreen.dart';
+import 'package:lecture30google_map_api/FourthMapScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,13 +18,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       // home: const FirstMapScreen(),
       // home: const SecondMapScreen(),
-      home: const ThirdMapScreen(),
+      // home: const ThirdMapScreen(),
+      // home: const FourthMapScreen(),
+      home: const FiveMapScreen(),
     );
   }
 }
